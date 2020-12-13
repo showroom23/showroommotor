675d20d69c508ceb1bed4d07cc6e9d569e52ed27
syawal <wpshowroom023@gmail.com>
2020-12-13 21:25:13 +0900

    master

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ app.set('view engine','ejs');
 
 app.use(bodyParser.urlencoded());
 app.use(express.static('public'));
+
+const IndexRoutes = require('./routers/index');
+const HondaRoutes = require('./routers/Honda');
+const KawasakiRoutes = require('./routers/Kawasaki');
+const ReserveRoutes = require('./routers/Reserve');
+const SuzukiRoutes = require('./routers/Suzuki');
+const YamahaRoutes = require('./routers/Yamaha');
+
+app.use('/index', IndexRoutes);
+
+app.listen(8085);
+
+console.log('tes');
