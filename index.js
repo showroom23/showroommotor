8b2d2f12d4806d79642969b26ad5452e66536994
FattahWG <caplin404@gmail.com>
2020-12-13 14:07:57 +0700

    master

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const layouts = require('express-ejs-layouts');
 
 const app = express();
 
@@ -7,3 +9,16 @@ app.set('view engine','ejs');
 
 app.use(bodyParser.urlencoded());
 app.use(express.static('public'));
+
+
+app.use(session({
+    cookie: {}
+}));
+
+app.use(layouts);
+app.set('layouts', 'layouts/index.ejs');
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+
+const indexRouter = require('./routers/index')
